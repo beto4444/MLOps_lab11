e335b6fb3a18fce7024a1ed9e99e9a282302d9e8
beto4444 <84237487+beto4444@users.noreply.github.com>
2026-01-06 21:54:03 +0100

    ex4 fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.scripts.settings import Settings
-from src.scripts.export_classifier_to_onnx import export_model_to_onnx
-from src.scripts.export_sentence_transformer_to_onnx import export_classifier_to_onnx
+from src.scripts.export_classifier_to_onnx import export_classifier_to_onnx
+from src.scripts.export_sentence_transformer_to_onnx import export_model_to_onnx                                                  
 from src.scripts.download_artifacts import download_artifacts
 
 if __name__ == "__main__":
